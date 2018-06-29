bc1257a73b1ca7936e362174d0a3db13b1ade6b8
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-06-29 10:48:16 -0500

    rendering context_data

--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
+'''
+def home(request):
+    return render(request, "countries/home.html")
+'''
+class HomeView(TemplateView):
+    template_name = 'countries/home.html' 
+    '''
+    def get(self, request, *args, **kwargs):
+        #return HttpResponse('GET request!')
+        return render(request, "countries/home.html")
+    '''
+    def get_context_data(self, *args, **kwargs):
+        colombia = {'name':'colombia', 'code': 'CO'}
+        usa = {'name':'estados unidos', 'code': 'USA'}
+        mexico = {'name':'mexico', 'code': 'MX'}
+        countries = [colombia, usa, mexico]
+        return {'countries': countries}
+
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST request!')
+
